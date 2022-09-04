0fc33e12f68ab7ec8c12bfd536867f3706566217
unknown <miso324@naver.com>
2022-09-04 21:32:07 +0900

    test5

--- database2.py
+++ database2.py
@@ -8,9 +8,10 @@ conn = pymysql.connect(host='smooth.mysql.pythonanywhere-services.com',
                     db='smooth$yampick',
                     charset='utf8')
 
-cur = conn.cursor(MySQLdb.cursors.DictCursor)
+cur = conn.cursor()
+# cur = conn.cursor(MySQLdb.cursors.DictCursor)
 
-cur.execute("""CREATE TABLE tb_user(
+cur.execute("""CREATE TABLE if not exists tb_user(
                     user_email VARCHAR(45) NOT NULL PRIMARY KEY,
                     user_pw VARCHAR(45) NOT NULL,
                     user_name VARCHAR(16) NOT NULL);
