423dee060773c4692cc2b984252507702c074a42
unknown <miso324@naver.com>
2022-09-04 23:07:12 +0900

    test6

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def login():
-    return "테스트 성공"
-    # return render_template()
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
