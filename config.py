047f20be2f6983f4d2d0dcd930e39b9e0385d9e5
unknown <miso324@naver.com>
2022-09-05 22:49:29 +0900

    test8

--- config.py
+++ config.py
@@ -2,11 +2,6 @@ from flask import Flask
 from sqlalchemy import create_engine
 
 
-
-
-app = Flask(__name__)
-app.config["DEBUG"] = True
-
 SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://{username}:{password}@{hostname}/{databasename}".format(
     username='smooth',
     password='weareking!',
@@ -17,3 +12,10 @@ SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://{username}:{password}@{hostname}/{dat
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # engine = create_engine(SQLALCHEMY_DATABASE_URI, pool_recycle=280)
+
+# import os
+
+# BASE_DIR = os.path.dirname(__file__)
+
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+# SQLALCHEMY_TRACK_MODIFICATIONS = False
--- yp/__init__.py
+++ yp/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+
 import config
 
 db = SQLAlchemy()
 migrate = Migrate()
+
 
 def create_app():
     app = Flask(__name__)
