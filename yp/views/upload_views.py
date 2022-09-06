8cf7d54fab296b463c22a7b1d0a5a412c7206246
unknown <miso324@naver.com>
2022-09-06 20:58:02 +0900

    1차완료

--- yp/views/upload_views.py
+++ yp/views/upload_views.py
@@ -32,24 +32,47 @@ def upload_done():
     global model_result
     model_result = ['김치찌개', '부대찌개', '등갈비', '냉면', '시리얼']
 
-    return render_template("upload_check.html", first = model_result[0])
+    return render_template("upload_check.html", first = model_result[0], photo = NEW_IMG.upload_location)
 
 @bp.route("/re")
 def double_check():
     if NEW_IMG:
-        return render_template("upload_check2.html", food_list = model_result[1:])
+        return render_template("upload_check2.html", food_list = model_result[1:], photo = NEW_IMG.upload_location)
 
     else:
         return "잘못된 접근입니다." # 에러 페이지 만들거나 홈으로 돌아가기
 
 
-@bp.route("/how")
+@bp.route("/how", methods=["POST"])
 def how():
-    print(request)
-    print(request.args)
-    NEW_IMG.upload_foodname = request.args.get("food_name")
-    NEW_IMG.upload_isnew = True
+    NEW_IMG.upload_foodname = request.form["food_name"]
+
+    # 이미 가지고 있는 음식 목록에 있다면
+    if NEW_IMG.upload_foodname in ['김치찌개', '된장찌개']:
+        NEW_IMG.upload_isnew = False
+    else:
+        NEW_IMG.upload_isnew = True
     db.session.add(NEW_IMG)
     db.session.commit()
-    return NEW_IMG.upload_foodname
+    return render_template("upload_how.html", photo = NEW_IMG.upload_location)
+
+@bp.route("/result", methods=["POST"])
+def result():
+    if NEW_IMG.upload_isnew:
+        return "해당 음식이 존재하지 않습니다."
+    else:
+        RATIO = float(request.form["ratio"])
+
+        # 해당 음식의 칼로리 가져오기
+        CAL = 5000
+
+        # 기초대사량 가져오기
+        user_cal = 7000
+
+        # 칼로리 계산
+        res = user_cal - 250 - CAL * RATIO
+
+        # 계산하는 코드
+
+        return render_template("upload_result.html")
 
