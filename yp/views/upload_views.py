170fec9a2476f847632a9aae52384332f8d0badb
unknown <miso324@naver.com>
2022-09-06 18:44:52 +0900

    8

--- yp/views/upload_views.py
+++ yp/views/upload_views.py
@@ -4,6 +4,7 @@ from yp import db
 from sqlalchemy import func
 
 bp = Blueprint('upload', __name__, url_prefix='/upload')
+NEW_IMG = None
 
 
 @bp.route('/')
@@ -12,24 +13,36 @@ def upload():
 
 @bp.route("/", methods=["POST"])
 def upload_done():
-    # NEW_IMG = tb_user_img(upload_user = '',
-    #                     upload_foodname = '',
-    #                     upload_location = '',
-    #                     upload_isnew = True)
     
+    global NEW_IMG
     NEW_IMG = tb_user_img()
-    print(db.session.query(func.count(tb_user_img.upload_index)).all()[0][0])
     NEW_IMG.upload_index = db.session.query(func.count(tb_user_img.upload_index)).all()[0][0] + 1
     NEW_IMG.upload_user = '지현'
-    NEW_IMG.upload_foodname = '된장찌개'
-    NEW_IMG.upload_location = f"yp/static/img/{NEW_IMG.upload_index}.jpeg"
-    NEW_IMG.upload_isnew = True
 
-    db.session.add(NEW_IMG)
-    
-    db.session.commit()
+    # if 가이드라인에 맞지 않음:
+        # return redirect(url_for("upload.upload"))
+
+    # else
+    NEW_IMG.upload_location = f"yp/static/img/{NEW_IMG.upload_index}.jpeg"
 
     uploaded_files = request.files["food_img"]
     uploaded_files.save(NEW_IMG.upload_location)
 
-    return redirect(url_for("upload.upload"))
+    # DS 결과
+    global model_result
+    model_result = ['김치찌개', '부대찌개', '등갈비', '냉면', '시리얼']
+
+    return render_template("upload_check.html", first = model_result[0])
+
+@bp.route("/re")
+def double_check():
+    if NEW_IMG:
+        
+        NEW_IMG.upload_foodname = '된장찌개'
+        NEW_IMG.upload_isnew = True
+        db.session.add(NEW_IMG)
+        db.session.commit()
+        return render_template("upload_check2.html", food_list = model_result[1:])
+    else:
+        return "잘못된 접근입니다." # 에러 페이지 만들거나 홈으로 돌아가기
+
